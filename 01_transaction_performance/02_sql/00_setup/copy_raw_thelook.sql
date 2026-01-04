a743731650ea2c2d6dbaf0b3a078abf6a2f8ccfd
brwibisono <br.wibisono@gmail.com>
2026-01-04 17:58:50 +0700

    Update copy_raw_thelook.sql

--- 01_transaction_performance/02_sql/00_setup/copy_raw_thelook.sql
+++ 01_transaction_performance/02_sql/00_setup/copy_raw_thelook.sql
@@ -6,15 +6,15 @@ DECLARE table_name STRING;
 
 FOR row IN (
   SELECT table_name
-  FROM `bigquery-public-data.thelook_ecommerce.INFORMATION_SCHEMA.TABLES`
+  FROM bigquery-public-data.thelook_ecommerce.INFORMATION_SCHEMA.TABLES
   WHERE table_type = 'BASE TABLE'
 )
 DO
   SET table_name = row.table_name;
 
   EXECUTE IMMEDIATE FORMAT("""
-    CREATE OR REPLACE TABLE `brwibisono.raw_thelook.%s` AS
-    SELECT * FROM `bigquery-public-data.thelook_ecommerce.%s`
+    CREATE OR REPLACE TABLE brwibisono.raw_thelook.%s AS
+    SELECT * FROM bigquery-public-data.thelook_ecommerce.%s
   """, table_name, table_name);
 
 END FOR;
