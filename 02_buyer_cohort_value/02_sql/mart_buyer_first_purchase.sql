813707d70624c37eea5783f0c7ff70d728280315
brwibisono <br.wibisono@gmail.com>
2026-01-18 13:42:03 +0700

    Delete mart_buyer_first_purchase.sql

--- 02_buyer_cohort_value/02_sql/mart_buyer_first_purchase.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TABLE brwibisono.analytics_thelook.mart_buyer_first_purchase AS
-SELECT
-  user_id,
-  MIN(order_date) AS first_order_date,
-  DATE_TRUNC(MIN(order_date), MONTH) AS first_purchase_month
-FROM brwibisono.analytics_thelook.clean_orders
-GROUP BY user_id;
