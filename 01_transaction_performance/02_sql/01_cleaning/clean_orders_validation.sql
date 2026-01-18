a26e41cf8531a150bd3621b96a91dde23a51e60e
brwibisono <br.wibisono@gmail.com>
2026-01-18 13:23:36 +0700

    Update clean_orders_validation.sql

--- 01_transaction_performance/02_sql/01_cleaning/clean_orders_validation.sql
+++ 01_transaction_performance/02_sql/01_cleaning/clean_orders_validation.sql
@@ -1,6 +1,1 @@
-SELECT
-  COUNT(*) AS total_orders,
-  COUNT(DISTINCT user_id) AS total_buyers,
-  MIN(order_created_at) AS min_order_date,
-  MAX(order_created_at) AS max_order_date
-FROM brwibisono.analytics_thelook.clean_orders;
+
