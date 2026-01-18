af0ce61f6e111625250c3acc4cfa6329239c0d14
brwibisono <br.wibisono@gmail.com>
2026-01-18 13:22:22 +0700

    Remove KPI validation query from mart_daily_kpi_validation.sql

--- 01_transaction_performance/02_sql/02_marts/mart_daily_kpi_validation.sql
+++ 01_transaction_performance/02_sql/02_marts/mart_daily_kpi_validation.sql
@@ -1,6 +1,1 @@
-SELECT
-  COUNT(*) AS total_days,
-  MIN(order_date) AS start_date,
-  MAX(order_date) AS end_date,
-  SUM(net_sales) AS total_net_sales
-FROM brwibisono.analytics_thelook.mart_daily_kpi;
+
